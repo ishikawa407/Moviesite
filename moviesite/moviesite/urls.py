1a80095301ce389127116b66b171fc3340aae727
shimeng <shimeng407@gmail.com>
2019-07-10 00:16:57 +0800

    Build the main website

---
+++ moviesite/moviesite/urls.py
@@ -0,0 +1,28 @@
+"""moviesite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.urls import path
+from mainsite import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^movies/', include('mainsite.urls')),
+    #url(r'^collect/', include('collector.urls')),
+    #url(r'^analytics/', include('analytics.urls')),
+    url(r'^admin/', admin.site.urls),
+    #url(r'^rec/', include('recommender.urls'))
+]
